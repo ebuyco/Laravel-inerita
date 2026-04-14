7db354d4e11b1278a5381d5287a40ba81a449ec3
e4nie <ernie@smithbox.com>
2026-04-15 00:17:56 +0800

    task:migration and seeder

---
+++ inertia/database/factories/ListingFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Listing>
+ */
+class ListingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            // 'title' => $this->faker->sentence,
+            // 'description' => $this->faker->paragraph,
+            // 'price' => $this->faker->randomNumber(5),
+            // 'beds' => $this->faker->randomNumber(1),
+            // 'baths' => $this->faker->randomNumber(1),
+            // 'area' => $this->faker->randomNumber(3),
+            // 'city' => $this->faker->city,
+            // 'code' => $this->faker->postcode,
+            // 'street' => $this->faker->streetName,
+            // 'street_nr' => $this->faker->randomNumber(3),
+            'beds' => fake()->numberBetween(1, 7),
+            'baths' => fake()->numberBetween(1, 7),
+            'area' => fake()->numberBetween(30, 400),
+            'city' => fake()->city(),
+            'code' => fake()->postcode(),
+            'street' => fake()->streetName(),
+            'street_nr' => fake()->numberBetween(10, 200),
+            'price' => fake()->numberBetween(50_000, 2_000_000),
+        ];
+    }
+}
